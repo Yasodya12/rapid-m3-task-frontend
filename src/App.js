4005b78c60bf66f367e866cbad7564e65bcb4cc8
Yasodya12 <rishikayasodya12@gmail.com>
2024-10-30 09:34:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div className="bg-blue-500 text-white font-bold p-4 rounded">
+        Hello, Tailwind CSS!
+      </div>
+  );
+}
+
+export default App;
